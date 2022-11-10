34aa77438b2c8eb94694ae6e93fd48184673e578
AdryDevs <alonsoadrian86@gmail.com>
2022-11-10 20:33:46 +0100

    order controllers done and some changes in AuthRouter

---
+++ controllers/OrdersControllers.js
@@ -0,0 +1,93 @@
+const db = require("../models");
+const orders = db.order;
+const Op = db.Sequelize.Op;
+
+const OrderController = {};
+
+//CRUD
+
+    //Order a movie or a tv show
+
+    OrderController.order = (req, res) => { 
+        if (!req.body.movieId) {
+        res.status(400).send({
+            message: "Content is empty"
+        });
+        return;
+        }
+        
+        const order = {
+        movieId: req.body.movieId,
+        userId: req.userId,
+        status: req.body.status ? req.body.status : false
+        };
+        
+        orders.create(order)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+            message:
+                err.message || "An error occurred while creating the order."
+            });
+        });
+    };
+
+    //Update order
+
+        OrderController.update = (req, res) => {
+            const id = req.params.id;
+            
+            orders.update(req.body, {
+                where: { id: id }
+            })
+                .then(num => {
+                if (num == 1) {
+                    res.send({
+                    message: "Order was updated successfully."
+                    });
+                } else {
+                    res.send({
+                    message: `Cannot update order with id=${id}. Maybe order was not found or req.body is empty!`
+                    });
+                }
+                })
+                .catch(err => {
+                res.status(500).send({
+                    message: "Error updating order with id=" + id
+                });
+                });
+            };
+
+    //Get all orders from user 
+
+    OrderController.findAll = (req, res) => {
+        orders.findAll({ where: { userId: req.userId } })
+            .then(data => {
+            res.send(data);
+            })
+            .catch(err => {
+            res.status(500).send({
+                message:
+                err.message || "An error occurred while retrieving orders."
+            });
+            });
+        };
+
+        //Get all orders 
+
+        OrderController.findAllAdminOnly = (req, res) => {
+            orders.findAll()
+                .then(data => {
+                res.send(data);
+                })
+                .catch(err => {
+                res.status(500).send({
+                    message:
+                    err.message || "An error occurred while retrieving orders."
+                });
+                });
+            };
+
+module.exports = OrderController;
---
+++ views/OrderRouter.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const router = express.Router();
+
+const OrderController = require('../controllers/OrderController');
+const { auth } = require('../middlewares/auth');
+
+
+
+router.post('/order', auth, OrderController.order);
+router.put('/update/:id', auth, OrderController.update);
+router.get('/', auth, OrderController.findAllAdminOnly);
+router.get('/user', auth, OrderController.findAll);
+
+module.exports = router;
+
+
