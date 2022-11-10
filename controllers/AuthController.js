1a3f5fec28dc739ec01f0aae9e6543ab2260f741
AdryDevs <alonsoadrian86@gmail.com>
2022-11-10 20:39:22 +0100

    auth bug solved

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,7 +1,7 @@
 const { user } = require('../models/index');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const authConfig = require('../config/auth.config');
+const authConfig = require('../config/auth');
 
 const AuthController = {};
 
--- views/UserRouter.js
+++ views/UserRouter.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-// const AuthController = require('../controllers/AuthController');
+ const AuthController = require('../controllers/AuthController');
 
-// router.post('/signin', AuthController.signIn);
-// router.post('/signup', AuthController.signUp);
+ router.post('/signin', AuthController.signIn);
+ router.post('/signup', AuthController.signUp);
 
 module.exports = router;
