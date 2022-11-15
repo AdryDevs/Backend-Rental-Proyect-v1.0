605112c0eb14bebaff73dbf182502aa0da0f1f63
AdryDevs <alonsoadrian86@gmail.com>
2022-11-15 10:50:44 +0100

    added cors for domain troubleshooting

--- index.js
+++ index.js
@@ -4,13 +4,25 @@ const morgan = require('morgan');
 const logger = require('./config/winston');
 const db = require('./db.js');
 const router = require('./router.js');
+const cors = require('cors');
 
 const app = express();
 const PORT = process.env.PORT || 3000; 
 
+//Options for CORS configuration
+let corsOptions = {
+    origin: "*",
+    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    // methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
+    preflightContinue: false,
+    // allowedHeaders: "Origin,X-Requested-With,Content-Type,Accept,Authorization",
+    optionsSuccessStatus: 204
+};
+
 //Middleware
 app.use(morgan('combined', { stream: logger.stream }));
 app.use(express.json());
+app.use(cors(corsOptions));
 
 //Routes
 app.use(router);
